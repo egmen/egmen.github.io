ac5145f5494b3d2b332515c861320358471e77c8
egmen <egmen@mail.ru>
2017-09-17 12:20:31 +0300

    [+] Каркассон.
    https://github.com/YodasWs/carcassonne

---
+++ carcassonne/js/app.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('deviceready', function() { $ = Zepto;
+	// Load device-specific styling
+	switch(device.platform) {
+	case 'WinCE': case 'Win32NT':
+		$('link').first().after('<link rel="stylesheet" href="css/windowsphone.css"/>');
+		break;
+	case 'iOS':
+		$('link').last().after('<link rel="stylesheet" href="css/ios.css"/>');
+		break;
+	case 'Android':
+	default:
+		$('link').last().after('<link rel="stylesheet" href="css/android.css"/>');
+		$('nav').addClass('btn-group-vertical').find('a').addClass('btn').addClass('btn-default');
+		break;
+	}
+
+	// Add Cross-Device Bootstrap Styling
+	$('input[type="submit"]').addClass('btn').addClass('btn-default');
+});
