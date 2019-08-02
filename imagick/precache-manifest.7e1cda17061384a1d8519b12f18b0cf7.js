7ee051788b9d9cfa2fea42c9ad0e47f37f7684d4
egmen <egmen@mail.ru>
2019-08-02 15:34:43 +0300

    [+] Обработка изображений

---
+++ imagick/precache-manifest.7e1cda17061384a1d8519b12f18b0cf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583541f043375dab0a77d5266c6d970f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c43bea94d36baf08a55",
+    "url": "/static/css/main.409a51b5.chunk.css"
+  },
+  {
+    "revision": "2ec00e2a296e6ff9bf33",
+    "url": "/static/js/2.a326f196.chunk.js"
+  },
+  {
+    "revision": "0c43bea94d36baf08a55",
+    "url": "/static/js/main.361fd02e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
